d7c27366991a74596e399a7ac394cbc24553f2bd
Destiny O'Neill <doneill1923@gmail.com>
2025-01-29 13:04:30 -0500

    tRYING THINGS OUT

---
+++ services/HelloWorld/setup.sh
@@ -0,0 +1,8 @@
+#echo cloning down textcat_goemotions...
+#spacy project clone tutorials/textcat_goemotions
+#echo Downloading spacy project assets...
+#cd textcat_goemotions
+#spacy project assets
+#cd ..
+
+echo "Saying Hello! To the world."
